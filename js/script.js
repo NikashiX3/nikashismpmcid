72f9428b29c403c67ef3c3e9848b3bef5c166ff6
Nikashi33 <naufalblue33@gmail.com>
2026-05-12 22:24:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const config = {
     serverInfo: {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "Nikashi SMP", /*Server name*/
-        serverIp: "nikashi.minecraftindonesial.my.id:22069", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp: "nikashi.minecraftindonesial.my.id:21004", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "1278672938064941126" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
